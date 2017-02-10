6178585f1fe0e769348bbade2b767c266b02d5f8
kryptn <kryptn@gmail.com>
2017-02-10 06:41:50 -0700

    Start  of math to handle the cells and neighboring cells

--- Assets/Scripts/Cells.cs
+++ Assets/Scripts/Cells.cs
@@ -6,14 +6,24 @@ using UnityEngine;
 public class Cells : MonoBehaviour {
 
     public GameObject config;
-    private List<List<List<Cell>>> grid;
 
+    private Dictionary<Vector3, Cell> grid;
 
+    private float CycleTime;
+    private Vector3 GridStart;
+    private Vector3 GridStop;
+    private float CellWidth;
+    private Vector3 scale {get { return new Vector3(CellWidth * .90f, CellWidth * .90f, CellWidth * .90f); } }
 
     // Use this for initialization
     void Start () {
-		
-	}
+        
+        var configObj = config.GetComponent<LifeConfig>();
+        CellWidth = configObj.CellWidth;
+        GridStart = configObj.GridStart - new Vector3(CellWidth*.5f, CellWidth*.5f, CellWidth*.5f);
+        GridStop = configObj.GridStop;
+        CycleTime = configObj.CycleTime;
+    }
 
     // Update is called once per frame
     void Update()
